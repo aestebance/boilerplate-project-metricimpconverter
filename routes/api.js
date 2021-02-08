6f01e9810a2925a67d5f640ea33b7e0770b67c1b
Alejandro Esteban Celdrán <aestebance@uoc.edu>
2021-02-08 12:35:24 +0100

    finalizing

--- routes/api.js
+++ routes/api.js
@@ -17,14 +17,31 @@ module.exports = function (app) {
 
   app.route('/api/convert')
     .get(function (req, res){
-      let input = req.query.input;
-      let initNum = convertHandler.getNum(input);
-      let initUnit = convertHandler.getUnit(input);
-      let returnNum = convertHandler.convert(initNum, initUnit);
-      let returnUnit = convertHandler.getReturnUnit(initUnit);
-      let toString = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
+        let input = req.query.input;
 
-      //res.json
+        let error = "";
+
+        if (convertHandler.getNum(input) === "invalid number") {
+            error = "invalid number";
+        }
+        if (convertHandler.getUnit(input) === "invalid unit") {
+            error = (error === "") ? "invalid unit" : "invalid number and unit";
+        }
+        if (error !== "") {
+            res.send(error);
+        }
+
+        let initNum = convertHandler.getNum(input);
+        let initUnit = convertHandler.getUnit(input);
+        let returnNum = convertHandler.convert(initNum, initUnit);
+        let returnUnit = convertHandler.getReturnUnit(initUnit);
+        let toString = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
+        res.json({
+          initNum: initNum,
+          initUnit: initUnit,
+          returnNum: returnNum,
+          returnUnit: returnUnit,
+          string: toString
+        });
     });
-
 };
